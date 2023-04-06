432738842f9e21b39de562f283cfac86cbb22143
Londeka Zikalala <londekazikalala@gmail.com>
2023-04-06 15:21:10 +0200

    index.html

--- Test in there/test.isFromCapeTown.js
+++ Test in there/test.isFromCapeTown.js
@@ -2,4 +2,14 @@ describe('isFromCapeTown', function(){
     it('should return true for registrations that start with "CA"',
     function(){
         assert.equal(isFromCapeTown('CA 123 908'), true)
-        assert.equal(isFromCapeTown('ca 123 908'), true)
+        assert.equal(isFromCapeTown('CA'), true)
+    })
+    it('should return false for empty strings and registrations that do not start with CA ',
+    
+    function(){
+        assert.equal(isFromCapeTown('CB 123 908'), false);
+        assert.equal(isFromCapeTown(''),false);
+        assert.equal(isFromCapeTown('ca 123 908'), false)
+    }
+    )
+})
--- isWeekday.js
+++ isWeekday.js
@@ -1,5 +1,11 @@
 
-function isWeekday(dayOfTheWeek){ 
+/*function isWeekday(dayOfTheWeek){ 
     return !dayOfTheWeek.startsWith("S")
-};
-         
+};*/
+function isWeekday(daysOfTheWeek){
+    if(daysOfTheWeek.startsWith('M')||daysOfTheWeek.startsWith('T')||daysOfTheWeek.startsWith('W')||daysOfTheWeek.startsWith('F') ){
+       return true}
+        else if (daysOfTheWeek.startsWith('S')||daysOfTheWeek.startsWith(''))
+        {return false}
+    }
+    
