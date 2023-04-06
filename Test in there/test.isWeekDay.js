86e92409a76080b88953f1e572668a15c02696c0
Londeka Zikalala <londekazikalala@gmail.com>
2023-04-06 09:56:38 +0200

    isWeekday

--- Test in there/test.isWeekDay.js
+++ Test in there/test.isWeekDay.js
@@ -1,18 +1,19 @@
-describe('isWeekDay', function(){
+describe('isWeekday', function(){
     it('should return true for weekdays and other words that do not start with "S" and false for weekends',
     function(){
        
-            assert.equal(isWeekDay('Monday'), true);
-            assert.equal(isWeekDay('Saturday'), false, "This is a Weekend");
-            assert.equal(isWeekDay('March'), true, 'This word does not start with S');
+            assert.equal(isWeekday('Monday'), true);
+            assert.equal(isWeekday('Saturday'), false, "This is a Weekend");
+            assert.equal(isWeekday('March'), true, 'This word does not start with S');
 
     }
     )
     it(
         'should not be case sensitive and return "true" for empty strings',
         function(){
-            assert.equal(isWeekDay('march'), true);
-            assert.equal(isWeekDay(''), true);
+            assert.equal(isWeekday('march'), true);
+            assert.equal(isWeekday('tuesDay'), true)
+            assert.equal(isWeekday(''), true);
         }
     )
     
